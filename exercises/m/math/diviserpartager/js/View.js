ad6c444b5251e2f3b777d48e04131e1c8237a434
Antoine Lecoustre <a.lecoustre@gmail.com>
2014-03-06 18:31:03 +0100

    Création de la div de l'exercice

--- exercises/m/math/diviserpartager/js/View.js
+++ exercises/m/math/diviserpartager/js/View.js
@@ -25,8 +25,63 @@ m.math.diviserpartager.View = function (mdl, div) {
     };
 
 // private methods
+    var build_col_exercise = function(view){
+
+// md and lg devices
+        var exercise_div_md_lg = $('<div/>', {
+            class: 'visible-md visible-lg',
+            style: 'background-color: #ffffff; padding: 10px; margin-bottom:10px; border-radius: 6px 6px 6px 6px'
+        });
+
+        //Creation de la classe row
+        var row_md_lg = $('<div/>', {
+            class: 'row'
+        });
+
+        //Div des colonnes
+        var col_left_md_lg = $('<div/>', {
+            class: 'col-md-8'
+        });
+        var col_right_md_lg = $('<div/>', {
+            class: 'col-md-4'
+        });
+
+        col_left_md_lg.appendTo(row_md_lg);
+        col_right_md_lg.appendTo(row_md_lg);
+        row_md_lg.appendTo(exercise_div_md_lg);
+        exercise_div_md_lg.appendTo(view);
+
+// xs and ms devices
+        var exercise_div_xs_ms = $('<div/>', {
+            class: 'visible-xs visible-sm',
+            style: 'background-color: #ffffff; padding: 10px; border-radius: 6px 6px 6px 6px'
+        });
+
+        //Creation de la classe row
+        var row_xs_ms = $('<div/>', {
+            class: 'row'
+        });
+
+        //Div des colonnes
+        var col_left_xs_ms = $('<div/>', {
+            class: 'col-xs-8'
+        });
+        var col_right_xs_ms = $('<div/>', {
+            class: 'col-xs-4'
+        });
+
+        col_left_xs_ms.appendTo(row_xs_ms);
+        col_right_xs_ms.appendTo(row_xs_ms);
+        row_xs_ms.appendTo(exercise_div_xs_ms);
+        exercise_div_xs_ms.appendTo(view);
+    };
+
     var init_div = function (view) {
         var button;
+
+
+        build_col_exercise(view);
+
 
         button = $('<a/>', {
             href: '#',
